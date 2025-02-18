67fa46e20cacc941eadd0aec18ff156a7f25cb00
pro-soo <rhackdtn@naver.com>
2025-02-18 20:03:09 +0900

    Google Login 실습

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,10 +2,12 @@ import Link from "next/link";
 import { useState } from "react";
 import { BiMenu } from "react-icons/bi";
 import { AiOutlineClose } from "react-icons/ai";
-
+import {useSession, signOut} from "next-auth/react";
 
 export default function Navbar(){
     const [isOpen, setIsOpen] = useState(false);
+    const {data, status} = useSession();
+
     return (
         <>
             <div className="navbar">
@@ -14,7 +16,12 @@ export default function Navbar(){
                     <Link href="/stores" className="navbar__list--item">맛집 목록</Link>
                     <Link href="/stores/new" className="navbar__list--item">맛집 등록</Link>
                     <Link href="/users/likes" className="navbar__list--item">찜한 가게</Link>
-                    <Link href="/users/login" className="navbar__list--item">로그인</Link>
+                {status === 'authenticated' ? (
+                    <button type="button" onClick={()=> signOut()}>
+                    로그아웃
+                </button>
+                ) : (
+                    <Link href="/api/auth/signin" className="navbar__list--item">로그인</Link>)}
                 </div>
                 {/* mobile button */}
                 <div role="presentation" className="navbar__button" onClick={()=> setIsOpen((val) => !val)}>
@@ -28,7 +35,7 @@ export default function Navbar(){
                     <Link href="/stores" className="navbar__list--item--mobile">맛집 목록</Link>
                     <Link href="/stores/new" className="navbar__list--item--mobile">맛집 등록</Link>
                     <Link href="/users/likes" className="navbar__list--item--mobile">찜한 가게</Link>
-                    <Link href="/users/login" className="navbar__list--item--mobile">로그인</Link>
+                    <Link href="/api/auth/signin" className="navbar__list--item--mobile">로그인</Link>
                     </div>
                 </div>
             )}
---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,18 @@
+import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+import GoolgleProvider from "next-auth/providers/google";
+
+const prisma = new PrismaClient();
+
+export const authOptions = {
+    adapter: PrismaAdapter(prisma),
+    // Configure one or more authentication providers
+    providers: [
+        GoolgleProvider({
+           clientId: process.env.GOOGLE_CLIENT_ID || "",
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+        }),
+    ],
+}
+export default NextAuth(authOptions);
