d19360fdad3bcc9ccce581d69b9aac5f79b34969
pro-soo <rhackdtn@naver.com>
2025-02-19 11:19:42 +0900

    kakao login 실습

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -3,6 +3,7 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
 import GoolgleProvider from "next-auth/providers/google";
 import NaverProvider from "next-auth/providers/naver";
+import KakaoProvider from "next-auth/providers/kakao";
 
 const prisma = new PrismaClient();
 
@@ -17,6 +18,10 @@ export const authOptions = {
         NaverProvider({
             clientId: process.env.NAVER_CLIENT_ID || "",
             clientSecret: process.env.NAVER_CLIENT_SECRET || "",
+        }),
+        KakaoProvider({
+            clientId: process.env.KAKAO_CLIENT_ID || "",
+            clientSecret: process.env.KAKAO_CLIENT_SECRET || "",
         }),
     ],
     pages: {
--- src/pages/users/login.tsx
+++ src/pages/users/login.tsx
@@ -31,19 +31,19 @@ export default function LoginPage(){
             <div className="mt-10 mx-auto w-full max-w-sm">
                 <div className="flex flex-col gap-3">
                     <button type="button"
-                            onClick={() => signIn("google")}
+                            onClick={() => signIn("google", {callbackUrl: "/"})}
                             className="text-white flex gap-2 bg-[#4285f4] hover:bg-[#4285f4]/90 font-medium rounded-lg w-full px-5 py-4 text-center items-center justify-center">
                        <AiOutlineGoogle className="w-6 h-6"/>
                         Sign in with Google
                     </button>
                     <button type="button"
-                            onClick={() => signIn("naver")}
+                            onClick={() => signIn("naver", {callbackUrl: "/"})}
                             className="text-white flex gap-3 bg-[#2db400] hover:bg-[#2db400]/90 font-medium rounded-lg w-full px-5 py-4 text-center items-center justify-center">
                        <SiNaver className="w-4 h-4"/>
                         Sign in with Naver
                     </button>
                     <button type="button"
-
+                            onClick={() => signIn("kakao", {callbackUrl: "/"})}
                             className="text-black flex gap-2 bg-[#fef01b] hover:bg-[#fef01b]/90 font-medium rounded-lg w-full px-5 py-4 text-center items-center justify-center">
                        <RiKakaoTalkFill className="w-6 h-6"/>
                         Sign in with Kakao
